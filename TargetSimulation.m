9de8b6e20e95f170eaf3ed2feda0abbf70285ba5
Thomas Wengerter <thomas.wengerter@gmail.com>
2020-04-23 21:58:17 +0200

    Iterate multiple Targets and save CSV

--- TargetSimulation.m
+++ TargetSimulation.m
@@ -12,57 +12,63 @@ clear
 global c_0;
 c_0 = 299792458;
 
-Pedestrian = 0;
-Bicycle = 0;
-Car = 0;
-synteticTarget = 1; %use Signal simulation for synt point targets in simulateSignal.m
+% Select number of target samples
+Pedestrians = 1;
+Bicycles = 0;
+Cars = 0;
+synteticTarget = 0; %use Signal simulation for synt point targets in simulateSignal.m
+
 
 %Generate Radar Object
 fmcw = FMCWradar;
 fmcw = fmcw.init_RDmap();
 
+
 %% Pedestrian Target
-if Pedestrian
+for target = 1:Pedestrians
     ped = backscatterPedestrian;
     %ped.Name = 'Pedestrian1';
     ped.Height = 1+rand(); % [1m,2m]
-    ped.WalkingSpeed = 1.4*ped.Height;
-%     ped.WalkingSpeed = 1; %in m/s
+    ped.WalkingSpeed = rand()* 1.4*ped.Height;
     ped.OperatingFrequency = fmcw.f0;
     ped.PropagationSpeed = c_0; %propagation speed of radar rays in air
-    posx = 25*rand();
-    posy = 25*rand();
-    ped.InitialPosition = [4; 0; 0]; %add random posx posy
-    ped.InitialHeading = 0; %in degree, heading along x from x=5 to x=7
+    randposx = fmcw.rangeBins(end)*rand();
+    ped.InitialPosition = [randposx; 0; 0]; %add random posx posy
+    randangle = rand()*360;
+    ped.InitialHeading = randangle; %in degree, heading along x from x=5 to x=7
     
     %Ground Truth
-    %targetR Range (dist. from radar to target)
-    %targetV radial Velocity
-    %targetV<0: approaching,    targetV>0: moving away from radar
+    %targetR: Range (radial dist. from radar to target)
+    %targetV: radial Velocity <0 approaching, targetV>0 moving away from radar
     targetR = sqrt(ped.InitialPosition(1)^2+ped.InitialPosition(2)^2);
     targetV = +ped.WalkingSpeed*cos(ped.InitialHeading/360*2*pi);
-    fprintf('Simulation of Pedestrian starting in Range %d m and radial Velocity %d m/s.\n', ...
-                targetR, targetV)
-    
     
     %Model Radar Signal for selected Target
     psb = modelSignal(ped, fmcw);
     pRD = fmcw.RDmap(psb);
-    fmcw.plotRDmap(pRD, [targetR, targetV]);
+    %fmcw.plotRDmap(pRD, [targetR, targetV]);
+    
+    %Label output and save
+    label = [targetR, targetV];
+    status = mkdir('SimulationData/Pedestrian');
+    writematrix(pRD, ['SimulationData/Pedestrian',num2str(target)]);
+    writematrix(label, ['SimulationData/Plabel',num2str(target)]);
 end
 
 
 %% Bycicle Traget
-if Bicycle
+for target = 1:Bicycle
     bike = backscatterBicyclist;
     bike.NumWheelSpokes = 20;
     bike.GearTransmissionRatio = 1.5; %Ratio of wheel rotations to pedal rotations
     bike.OperatingFrequency = fmcw.f0;
-    posx = 25*rand();
+    randposx = fmcw.rangeBins(end)*rand();
     posy = 25*rand();
-    bike.InitialPosition = [5;0;0];
-    bike.InitialHeading = 0; %in degree, heading along x-axis
-    bike.Speed = 3; %m/s
+    bike.InitialPosition = [randposx;0;0];
+    randangle = rand()*360;
+    bike.InitialHeading = randangle; %in degree, heading along x-axis
+    randspeed = rand()*fmcw.velBins(end);
+    bike.Speed = randspeed; %m/s
     bike.Coast = false; %Padeling movements?
     bike.PropagationSpeed = c_0; %propagation speed of radar rays in air
     % bike.AzimutAngles = fmcw.azimut; %default 77GHz cyclist <- use default
@@ -70,41 +76,53 @@ if Bicycle
     % bike.RCSPattern = fmcw.RCS; %default 77GHz cyclist <- use default
     
     %Ground Truth
-    %targetR Range (dist. from radar to target)
-    %targetV radial Velocity
-    %targetV<0: approaching,    targetV>0: moving away from radar
+    %targetR: Range (radial dist. from radar to target)
+    %targetV: radial Velocity <0 approaching, targetV>0 moving away from radar
     targetR = sqrt(bike.InitialPosition(1)^2+bike.InitialPosition(2)^2);
     targetV = +bike.Speed*cos(bike.InitialHeading/360*2*pi);
-    fprintf('Simulation of Bycicle starting in Range %d m and radial Velocity %d m/s.\n', ...
-                targetR, targetV)
-    
             
     %Model Radar Signal for selected Target
     bsb = modelSignal(bike, fmcw);
     bRD = fmcw.RDmap(bsb);
-    fmcw.plotRDmap(bRD, [targetR, targetV]);
+    %fmcw.plotRDmap(bRD, [targetR, targetV]);
+    
+    %Label output and save
+    label = [targetR, targetV];
+    status = mkdir('SimulationData/Bicycle');
+    writematrix(bRD, ['SimulationData/Bicycle',str(target)]);
+    writematrix(label, ['SimulationData/Blabel',str(target)]);
 end
 
-if Car
-    targetsRange = [10];
-    targetsVelocity = [5];
+for target = 1:Cars
+    targetR = [10]; %add more with ;
+    targetV = [5]; %add more with ;
     
-    csb = simulateSignal(fmcw, targetsRange, targetsVelocity, 0, false);
+    csb = simulateSignal(fmcw, targetR, targetV, 0, false);
     cRD = fmcw.RDmap(csb);
-    fmcw.plotRDmap(cRD, [targetsRange, targetsVelocity]);
+    fmcw.plotRDmap(cRD, [targetR, targetV]);
     
+    %Label output and save
+    label = [targetR, targetV];
+    status = mkdir('SimulationData/Car');
+    writematrix(cRD, ['SimulationData/Car',str(target)]);
+    writematrix(label, ['SimulationData/Car',str(target)]);
 end
 
-if synteticTarget
+for target = 1:synteticTarget
     %Simulate syntetic point targets
     %Enter Positions/Velocities for point targets
-    targetsRange = [10];
-    targetsVelocity = [5];
+    targetR = [10]; %add more with ;
+    targetV = [5]; %add more with ;
     
-    csb = simulateSignal(fmcw, targetsRange, targetsVelocity, 0, false);
+    csb = simulateSignal(fmcw, targetR, targetV, 0, false);
     cRD = fmcw.RDmap(csb);
-    fmcw.plotRDmap(cRD, [targetsRange, targetsVelocity]);
+    fmcw.plotRDmap(cRD, [targetR, targetV]);
     
+    %Label output and save
+    label = [targetR, targetV];
+    status = mkdir('SimulationData/Syntetic');
+    writematrix(bRD, ['SimulationData/Syntetic',str(target)]);
+    writematrix(label, ['SimulationData/Slabel',str(target)]);
 end
 
 
